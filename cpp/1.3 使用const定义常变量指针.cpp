b2c0e53ab4bbf0ff6277cc029c42bc7ac6b8c425
Mr_Right <80014073+Mr-Righter@users.noreply.github.com>
2022-04-08 23:16:32 +0800

    Delete cpp directory

--- cpp/1.3 使用const定义常变量指针.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	char* const str = new char[4]{'a','b','c','\0'};   //定义指针常量，此时指针的值（指向的地址）无法改变
-	str[0] = 'z';
-	cout << str;          //cout能直接输出字符数组（前提是字符数组以'\0'结束，否则会有乱码），但不能直接输出int型数组
-	return 0;
-}
