ac6f7ad7591f9b1e79d0ce425206bedaba16c9e2
Mr_Right <80014073+Mr-Righter@users.noreply.github.com>
2022-04-08 23:17:53 +0800

    Add files via upload

---
+++ C++面向对象程序设计实验/PPT/cpp/1.1 定义和使用动态指针数组.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cin >> x;
+	int** num = new int*[x];           //创建一个动态指针数组
+	for (int i = 0; i < x; i++)
+	{
+		num[i] = new int;              //动态创建int类型的变量
+		cin >> *num[i];
+	}
+	int sum = 0;
+	for (int i = 0; i < x; i++)
+	{
+		sum = sum + *num[i];
+		delete num[i];                 
+	}
+	cout << sum;
+	delete[]num;
+	return 0;
+}
